3e03b3f6c6554bbd34545a9789687d0e7fc1a8c1
Antônio Sousa <antonioaads@gmail.com>
2021-06-22 23:34:05 -0300

    Exercicio 3 - done

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,12 @@
-// Faça o exercício dos PARÁGRAFOS aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+const botoes = document.querySelectorAll('.botao-expandir-retrair');
+
+function toggleP(e) {
+    const clickedButton = e.currentTarget;
+    const parent = clickedButton.parentNode;
+    const isExpanded = parent.classList.toggle('expandido');
+    clickedButton.innerHTML = isExpanded ? '-' : '+';
+}
+
+for (let botaoEl of botoes) {
+    botaoEl.addEventListener('click', toggleP);
+}
