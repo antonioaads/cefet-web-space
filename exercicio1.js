da4d54bcebe8db43967e0a47ac1b22df2285b914
Antônio Sousa <antonioaads@gmail.com>
2021-06-22 22:42:17 -0300

    Exercicio 1 - done

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,12 @@
-// Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+const botaoCalcular = document.querySelector('#calcular');
+
+botaoCalcular.addEventListener('click', function(){
+    const constante = document.querySelector('#constante').value;
+    const massa1 = document.querySelector('#massa1').value;
+    const massa2 = document.querySelector('#massa2').value;
+    const distancia = document.querySelector('#distancia').value;
+
+    const resultado = document.querySelector('#resultado');
+
+    resultado.value = (constante * massa1 * massa2)/(distancia * distancia); // poderia usar pow
+})
